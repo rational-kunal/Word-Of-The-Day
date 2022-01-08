ab7afe01b3d3d67eb84229c5498cd73c8b663596
Kunal Kamble <kunalgk1999@gmail.com>
2022-01-08 20:55:01 +0530

    🚧 Added support to render Word of the Day
    - Moved `parcel` to dev dependencies.
    - Implemented simple scrapper to get the Word of the Day from Merriam Webster website.
    - Added minimal tags to render the Word of the Day.

--- src/script.js
+++ src/script.js
@@ -1,5 +1,22 @@
+import { getWordOfTheDay } from './wordy'
+
+const WORD_MAIN_ID = 'word-main'
+const WORD_ATTRIBUTE_ID = 'word-attr'
+const WORD_SYLLABLES_ID = 'word-syllables'
+const WORD_MEANING_ID = 'word-meaning'
+
 function onLoad() {
-  console.log('Hello World')
+  const wordMainEl = document.getElementById(WORD_MAIN_ID)
+  const wordAttributeEl = document.getElementById(WORD_ATTRIBUTE_ID)
+  const wordSyllablesEl = document.getElementById(WORD_SYLLABLES_ID)
+  const wordMeaningEl = document.getElementById(WORD_MEANING_ID)
+
+  getWordOfTheDay().then((wordOfTheDay) => {
+    wordMainEl.innerText = wordOfTheDay.word
+    wordAttributeEl.innerText = wordOfTheDay.attribute
+    wordSyllablesEl.innerText = wordOfTheDay.syllables
+    wordMeaningEl.innerHTML = wordOfTheDay.meaning
+  })
 }
 
 onLoad()
---
+++ src/wordy.js
@@ -0,0 +1,36 @@
+const WORD_OF_THE_DAY_URL = 'https://www.merriam-webster.com/word-of-the-day'
+const WORD_OF_THE_DAY_QUERY = {
+  word: 'div.quick-def-box div.word-header div.word-and-pronunciation h1',
+  attribute: 'div.quick-def-box div.word-attributes span.main-attr',
+  syllables: 'div.quick-def-box div.word-attributes span.word-syllables',
+  meaning: 'div.wod-article-container div.wod-definition-container p',
+  example:
+    'div.wod-article-container div.wod-definition-container div.wotd-examples div.left-content-box p',
+}
+
+const getWordOfTheDay = async () => {
+  const wordOfTheDayRequest = new Request(WORD_OF_THE_DAY_URL)
+  const response = await fetch(wordOfTheDayRequest)
+  const responseText = await response.text()
+
+  let parser = new DOMParser()
+  let doc = parser.parseFromString(responseText, 'text/html')
+
+  const innerTextForQuery = (query) => {
+    return doc.querySelector(query).innerHTML
+  }
+
+  const htmlForQuery = (query) => {
+    return doc.querySelector(query)
+  }
+
+  return {
+    word: innerTextForQuery(WORD_OF_THE_DAY_QUERY.word),
+    attribute: innerTextForQuery(WORD_OF_THE_DAY_QUERY.attribute),
+    syllables: innerTextForQuery(WORD_OF_THE_DAY_QUERY.syllables),
+    meaning: innerTextForQuery(WORD_OF_THE_DAY_QUERY.meaning), // TODO: Get the complete meaning
+    example: innerTextForQuery(WORD_OF_THE_DAY_QUERY.example),
+  }
+}
+
+export { getWordOfTheDay }
