004dd0d0e22c8d67a1e37ea0b1fd525aa79dc7f5
Kunal Kamble <kunalgk1999@gmail.com>
2022-01-09 15:21:25 +0530

    🚧 [MVP] Stylized the Page
    - Added box layout support with some styles.
    - Styles and color inspired by: https://dribbble.com/shots/7792683-Word-of-the-Day-Oct-24

--- src/script.js
+++ src/script.js
@@ -4,18 +4,24 @@ const WORD_MAIN_ID = 'word-main'
 const WORD_ATTRIBUTE_ID = 'word-attr'
 const WORD_SYLLABLES_ID = 'word-syllables'
 const WORD_MEANING_ID = 'word-meaning'
+const WORD_DATE_ID = 'word-date'
+const WORD_EXAMPLE_ID = 'word-example'
 
 function onLoad() {
   const wordMainEl = document.getElementById(WORD_MAIN_ID)
   const wordAttributeEl = document.getElementById(WORD_ATTRIBUTE_ID)
   const wordSyllablesEl = document.getElementById(WORD_SYLLABLES_ID)
   const wordMeaningEl = document.getElementById(WORD_MEANING_ID)
+  const wordDateEl = document.getElementById(WORD_DATE_ID)
+  const wordExampleEl = document.getElementById(WORD_EXAMPLE_ID)
 
   getWordOfTheDay().then((wordOfTheDay) => {
+    wordDateEl.innerText = new Date().toDateString()
     wordMainEl.innerText = wordOfTheDay.word
     wordAttributeEl.innerText = wordOfTheDay.attribute
     wordSyllablesEl.innerText = wordOfTheDay.syllables
     wordMeaningEl.innerHTML = wordOfTheDay.meaning
+    wordExampleEl.innerHTML = wordOfTheDay.example
   })
 }
 
